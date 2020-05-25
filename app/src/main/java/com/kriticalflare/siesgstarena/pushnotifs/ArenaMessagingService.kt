4b0a6c1cd369d2455381e309ebe5827a03a55072
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2020-05-25 21:10:49 +0530

    Add firebase push notifications for upcoming contests

---
+++ app/src/main/java/com/kriticalflare/siesgstarena/pushnotifs/ArenaMessagingService.kt
@@ -0,0 +1,73 @@
+package com.kriticalflare.siesgstarena.pushnotifs
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.kriticalflare.siesgstarena.R
+import com.kriticalflare.siesgstarena.ui.MainActivity
+import kotlin.random.Random
+
+class ArenaMessagingService : FirebaseMessagingService(){
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "onNewToken: $token")
+
+        // Send the token to server
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            //Message Services handle notification
+            showNotification(it.title!!,it.body!!, context = applicationContext)
+        }
+    }
+
+    private fun showNotification(title:String, message: String , context: Context){
+        val intent = Intent(context, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            context, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val channelId = context.getString(R.string.contests_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(context, channelId)
+            .setColor(ContextCompat.getColor(context, android.R.color.holo_red_dark))
+            .setSmallIcon(R.drawable.ic_code_black_24dp)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                context.getString(R.string.contests_channel_name),
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(Random.nextInt(), notificationBuilder.build())
+    }
+
+    companion object {
+        const val TAG = "ArenaMessagingService"
+    }
+}
