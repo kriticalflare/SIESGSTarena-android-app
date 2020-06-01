6a7c1e0bd96b3c339ed58aaba9a28ca2b96c8f19
kriticalflare <42350771+kriticalflare@users.noreply.github.com>
2020-06-01 20:06:59 +0530

    Add proper splash screen

--- app/src/main/java/com/kriticalflare/siesgstarena/ui/MainActivity.kt
+++ app/src/main/java/com/kriticalflare/siesgstarena/ui/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val root = binding.root
